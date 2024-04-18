5e2d03c48c80c557b3d3b67747fc289936d9d501
Atharv Joundal <158443883+Atharv2433@users.noreply.github.com>
2024-04-18 18:52:29 +0530

    Create index.js

---
+++ fs Module/index.js
@@ -0,0 +1,36 @@
+// File operations using node JS
+//CURD Operation using nodejs [fs module] 
+
+const fs = require("fs");
+
+// Read Operation
+let data = fs.readFileSync("read.txt");
+data = data.toString();
+console.log(data);
+
+// Write Operation
+fs.writeFileSync("abc.txt", "I am Atharv Joundal Pursuing BE in Computer Engineering ");
+
+// Append operation
+fs.appendFileSync(
+  "abc.txt",
+  "From Dr. D. Y. Patil College of Engineering Akurdi, Pune."
+);
+
+// Rename Operation
+fs.renameSync("abc.txt", "xyz.txt");
+
+// Read Operation
+let data1 = fs.readFileSync("xyz.txt");
+data1 = data1.toString();
+console.log(data1);
+
+// Path of the file
+console.log(fs.realpathSync("xyz.txt"));
+
+// Read Operation
+const datas = fs.readFileSync("pqr.txt", "utf-8");
+
+// For Deleting the file and Directory
+fs.unlinkSync("pqr.txt");
+fs.rmdirSync("data");
