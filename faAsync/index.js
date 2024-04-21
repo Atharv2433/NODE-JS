cf8697c4034513bb6b6475d13ffdc411b86739dd
Atharv Joundal <158443883+Atharv2433@users.noreply.github.com>
2024-04-21 23:22:59 +0530

    Create index.js

---
+++ faAsync/index.js
@@ -0,0 +1,30 @@
+
+const fs = require("fs");
+
+fs.mkdir("ABCD", (err) => {
+  console.log("Folder created Successfully");
+});
+
+fs.writeFile("./ABCD/abc.txt", "Hi Myself Atharv Joundal ", (err) => {
+  console.log("File created Successfully");
+});
+
+fs.appendFile("./ABCD/abc.txt", "I am pursuing BE from Dr. D. Y. Patil College of Engineering Akurdi Pune ", (err) => {
+  console.log("Data appended Successfully");
+});
+
+fs.readFile("./ABCD/abc.txt", "utf-8", (err, data) => {
+  console.log(data);
+});
+
+fs.rename("./ABCD/abc.txt", "./ABCD/xyz.txt", (err) => {
+  console.log("File Renamed Successfully");
+});
+
+fs.unlink("./ABCD/xyz.txt", (err) => {
+  console.log("File Deleted Successfully");
+});
+
+fs.rmdir("./ABCD", (err) => {
+  console.log("Folder Deleted Successfully ");
+});
